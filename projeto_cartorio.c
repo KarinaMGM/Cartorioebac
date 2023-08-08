7037cf82bc59d516d2286ccc89db88be1c3cd22b
KarinaMGM <140670360+KarinaMGM@users.noreply.github.com>
2023-08-08 00:13:45 -0300

    Projeto Final Cartório EBAC
    Projeto final Cartório EBAC, 100% funcional.

--- projeto_cartorio.c
+++ projeto_cartorio.c
@@ -15,17 +15,17 @@ int incluir() //Função responsável por cadastrar os usuários no sistema
    	 
    	printf("Digite o CPF: "); //Coletando informações do usuário
    	scanf("%s", cpf); //%s refere-se a string, salva a informação na strin, variável CPF
-   	
-   	
+
    	strcpy(arquivo, cpf); //Responsável por copiar os valores das String
    	
    	FILE *file; //Cria o arquivo 
    	file = fopen(arquivo, "w"); //Abre o arquivo e o "w" significa escrever-write
+   	fprintf(file, "CPF: "); //Adiciona "CPF" antes do cpf
    	fprintf(file, cpf); //Salva o valor da variável
    	fclose(file); //Fecha o arquivo
    	
     file = fopen(arquivo, "a"); //Abre o arquivo que foi salvo e o "a" significa atualizar  
-    fprintf(file, ","); //Adiciona "," depois do cpf
+    fprintf(file, ", Nome: "); //Adiciona "Nome" antes do nome
     printf("\n"); //Função de pular uma linha
     fclose(file); //Fecha o arquivo
     
@@ -37,7 +37,7 @@ int incluir() //Função responsável por cadastrar os usuários no sistema
    	fclose(file); //Fecha o arquivo
    	
    	file = fopen(arquivo, "a"); //Abre o aquivo que foi salvo, e atualiza
-    fprintf(file, ","); //Adiciona "," depois do nome
+    fprintf(file, ", Sobrenome: "); //Adiciona "sobrenome" antes do sobrenome
     printf("\n"); //Função de pular uma linha
     fclose(file); //Fecha o arquivo
     
@@ -49,7 +49,7 @@ int incluir() //Função responsável por cadastrar os usuários no sistema
     fclose(file); //Fecha o arquivo
     
     file = fopen(arquivo, "a"); //Abre o arquivo 	ue foi salvo e atualiza
-    fprintf(file, ","); //Adiciona a "," depois do sobrenome
+    fprintf(file, ", Cargo: "); //Adiciona a "cargo" antes do cargo
     printf("\n"); //Função de pular uma linha
     fclose(file); //Fecha o arquivo
     
@@ -60,11 +60,11 @@ int incluir() //Função responsável por cadastrar os usuários no sistema
     fprintf(file, cargo); //Salva o valor da variável
     fclose(file); //Fecha o arquivo
     printf("\n");
-
     
-    system("pause"); //Pausa a tela para o usuário ler a mensagem - pressione uma tecla para continuar
-    
-   		   
+    system("pause"); //Pausa a tela para o usuário ler a mensagem - pressione uma tecla para continuar	
+	
+	system("cls");	   
+	
 }  
 
 int consultar() //Função responsável por consultar os usuários no sistema
@@ -74,7 +74,7 @@ int consultar() //Função responsável por consultar os usuários no sistema
    char cpf[40];
    char conteudo[200];
    
-   printf("Digite o CPF: \n"); //Coletando informações do usuário
+   printf("Digite o CPF: "); //Coletando informações do usuário
    scanf("%s", cpf);//Salva a informação na string, variável CPF
    
    FILE *file; //Cria o arquivo
@@ -82,18 +82,20 @@ int consultar() //Função responsável por consultar os usuários no sistema
    
    if(file == NULL) //Análise de usuário inexistente
    {
-   printf("Registro não encontrado!\n\n");
+       printf("Registro não encontrado!\n\n");
    }
    
    while(fgets(conteudo, 200, file ) != NULL) // Análise de usuário existente
    {
-   printf("\nEssas são as informações do usuário: \n\n");
-   printf("%s", conteudo);
-   printf("\n\n");  
-   fclose(file); 
+       printf("\nEssas são as informações do usuário: \n\n");
+       printf("%s", conteudo);
+       printf("\n\n");  
+       fclose(file); 
    }
    
    system("pause"); //Pausa a tela para o usuário ler a mensagem - pressione uma tecla para continuar
+   
+   system("cls");
    
 }  
  
@@ -122,61 +124,73 @@ int deletar() //Função responsável por deletar os usuários no sistema
 		printf("Usuário deletado com sucesso!\n\n");
 		system("pause");
     }  
-
+     system("cls");
 }
 
 
 int main () //Função princial, sempre a primeira a ser executada
-    {
+{
 	
 	int opcao=0; //Definindo variáveis
 	int laco=1; // Criando variável Laço de repetição
+	char senhadigitada[10]="a";
+	int comparacao; //Definindo variavel de comparação
 	
-	for(laco=1;laco=1;) // Repetição, sempre volta para o menu inicial
+	printf("*** Banco de Dados da EBAC ***\n\n");
+	printf("Login de Administrador!!\n\nDigite sua senha: ");
+	scanf("%s",senhadigitada);
 	
-	{ //Início do laço
-		
-		system("cls"); //Responsável por limpar a tela
+	comparacao = strcmp(senhadigitada, "admin"); //comparando a sting
 	
-	    setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem - no português o sistema assume os acentos
+	if(comparacao == 0) //Se a senha digitada for "admin"
+	{
+	   system("cls");
+	   	
+	   for(laco=1;laco=1;) // Repetição, sempre volta para o menu inicial
 	
-	    printf("*** Banco de Dados da EBAC ***\n\n"); //Início do menu
-	    printf("Escolha a opção desejada:\n\n");
-	    printf("\t1- Incluir Usuário\n");
-	    printf("\t2- Consultar Usuário\n");
-	    printf("\t3- Deletar Usuário\n");
-	    printf("\t4- Sair do Sistema\n\n\n");
-	    printf("Opção: "); //Fim do Menu
+	    {  //Início do laço			
+	         setlocale(LC_ALL, "Portuguese"); //Definindo a linguagem - no português o sistema assume os acentos
 	
-	    scanf("%d", &opcao); //Armazenando a escolha do usuário
+	         printf("*** Banco de Dados da EBAC ***\n\n"); //Início do menu
+	         printf("Escolha a opção desejada:\n\n");
+	         printf("\t1- Incluir Usuário\n");
+	         printf("\t2- Consultar Usuário\n");
+	         printf("\t3- Deletar Usuário\n");
+	         printf("\t4- Sair do Sistema\n\n\n");
+	           printf("Opção: "); //Fim do Menu
 	
-    	system("cls"); // limpar a tela 
+	        scanf("%d", &opcao); //Armazenando a escolha do usuário
 	
-	    switch(opcao) //Início da variável
-	    {
-	    	case 1: //opção de seleção do menu
-	    	incluir(); //Chamada de funções
-	        break; // Fecha a seleção 
+    	    system("cls"); // limpar a tela 
+	
+	         switch(opcao) //Início da variável
+	        {
+	         	case 1: //opção de seleção do menu
+	    	    incluir(); //Chamada de funções
+	            break; // Fecha a seleção 
 	        
-	        case 2: // opção de seleção do menu
-	        consultar(); //Chamada de funções
-		    break; // Fecha a seleção
+	            case 2: // opção de seleção do menu
+	            consultar(); //Chamada de funções
+		        break; // Fecha a seleção
 			
-			case 3: //opção de seleção do menu
-		    deletar(); //Chamada de funções
-    		break; //Fecha a seleção
+			    case 3: //opção de seleção do menu
+		        deletar(); //Chamada de funções
+    		    break; //Fecha a seleção
     		
-    		case 4: //opção de seleção do menu
-    		printf("Obrigado por utilizar o sistema!\n");
-            return 0; //sai do sistema
-            break; //Fecha a seleção
+    		    case 4: //opção de seleção do menu
+    		    printf("Obrigado por utilizar o sistema!\n");
+                return 0; //sai do sistema
+                break; //Fecha a seleção
     	
-    		default: //Análise de opção inexistente
-			printf("Essa opcão não está disponível!\n"); // Aviso ao usuário
-			system("pause"); // Pausa a tela para o usuário ler a mensagem - pressione uma tecla para continuar
-			break; // Fim da seleção
-		}
-	
-	
-    } //Fim do laço 	
-   }
+    		    default: //Análise de opção inexistente
+			    printf("Essa opcão não está disponível!\n"); // Aviso ao usuário
+			    system("pause"); // Pausa a tela para o usuário ler a mensagem - pressione uma tecla para continuar
+			    break; // Fim da seleção
+	     	}
+	    }//Fim do laço 	
+    }
+    
+    else
+        printf("Senha Incorreta!\n");
+    
+}
